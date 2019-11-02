38ed3ed5792a26bc47589918327996f817737cab
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-11-02 11:47:28 +0700

    Update todo with fetch

--- App.js
+++ App.js
@@ -28,7 +28,21 @@ export default function App() {
         renderItem={({ item }) => (
           <List.Item
             onPress={() => {
-              console.log("pressed!");
+              fetch(
+                `https://ancient-reaches-80096.herokuapp.com/todos/${item.id}/`,
+                {
+                  method: "PATCH",
+                  headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                  },
+                  body: JSON.stringify({ completed: !item.completed })
+                }
+              )
+                .then(response => response.json())
+                .then(data => {
+                  fetchTodos();
+                });
             }}
             title={item.title}
             left={props => (
